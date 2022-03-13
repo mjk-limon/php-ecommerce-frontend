014525f9789e97150949c1513b17bef72e43ec3f
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-03-13 19:12:28 +0600

    Footer changed

--- layouts/footer.php
+++ layouts/footer.php
@@ -11,24 +11,57 @@ $shippingMethods = $this->shippingMethods();
 <footer id="footer">
     <div class="footer-widget">
         <div class="container">
-            <div class="row">
-                <div class="col-md-2 col-xs-6">
+            <div class="row flex" style="flex-wrap:wrap;">
+                <div class="col-md-3 col-xs-6 flex">
                     <div class="single-widget">
-                        <h2>Company</h2>
+                        <img src="<?php echo Models::getLogo() ?>" class="img-responsive">
+                        <p style="margin-top: 20px"></p>
+                        <div class="social-icons text-center footer-social">
+                            <ul class="nav">
+
+                                <?php foreach ($socialLinks as $SK => $SL) : ?>
+                                    <li>
+                                        <a href="<?php echo $SL ?>" target="_blank">
+                                            <i class="fa fa-<?php echo $SK ?>"></i>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <div class="single-widget">
+                        <h2>Contact Us</h2>
                         <ul class="nav nav-pills nav-stacked">
-                            <li><i class="fa fa-chevron-right"></i><a href="/page/about-us/">About Us</a></li>
-                            <li><i class="fa fa-chevron-right"></i><a href="/page/term-of-use/">Terms and Conditions</a></li>
-                            <li><i class="fa fa-chevron-right"></i><a href="/page/payment-methods/">How to Pay</a></li>
-                            <li><i class="fa fa-chevron-right"></i><a target="_blank" href="https://blog.<?php echo $_SERVER['HTTP_HOST'] ?>">Blog</a></li>
-                            <li><i class="fa fa-chevron-right"></i><a href="/page/photo-confirmations/">Photo Confirmations</a></li>
+                            <li>
+                                <p>
+                                    <i class="fa fa-map-marker contact-icon"></i>
+                                    <?php echo Models::getContactInformation('address') ?>
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    <i class="fa fa-mobile contact-icon"></i>
+                                    <?php echo Models::getContactInformation('mobile1') ?>
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    <i class="fa fa-envelope-o contact-icon"></i>
+                                    <?php echo Models::getContactInformation('email') ?>
+                                </p>
+                            </li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-2 col-xs-6">
                     <div class="single-widget">
-                        <h2>Support</h2>
+                        <h2>USEFULL LINKS</h2>
                         <ul class="nav nav-pills nav-stacked">
-                            <li><i class="fa fa-chevron-right"></i><a href="/contact/">Customer Care</a></li>
+                            <li><i class="fa fa-chevron-right"></i><a href="/page/about-us/">About Us</a></li>
+                            <li><i class="fa fa-chevron-right"></i><a href="/page/term-of-use/">Terms &amp; Conditions</a></li>
                             <li><i class="fa fa-chevron-right"></i><a href="/page/privacy-policy/">Privacy Policy</a></li>
                             <li><i class="fa fa-chevron-right"></i><a href="/page/shipping-returns/">Shipping Returns</a></li>
                             <li><i class="fa fa-chevron-right"></i><a href="/page/store-location/">Store Location</a></li>
@@ -36,38 +69,12 @@ $shippingMethods = $this->shippingMethods();
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-2 col-xs-6">
-                    <div class="single-widget">
-                        <h2>Account</h2>
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><i class="fa fa-chevron-right"></i><a href="/my-account/">User Account</a></li>
-                            <li><i class="fa fa-chevron-right"></i><a href="/my-account/?c=90.02">User Wishlists</a></li>
-                            <li><i class="fa fa-chevron-right"></i><a href="/my-account/?c=90.03">Order history</a></li>
-                            <li><i class="fa fa-chevron-right"></i><a href="/my-account/?c=90.04">My Reviews</a></li>
-                            <li><i class="fa fa-chevron-right"></i><a href="/page/testimonials/">Testimonials</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-2 hidden-xs"></div>
-                <div class="col-md-4 col-xs-6">
+
+                <div class="col-md-3 col-xs-6">
                     <div class="single-widget al-l">
-                        <h2>FOLLOW US</h2>
-                        <div class="social-icons">
-                            <ul class="nav">
-
-                                <?php foreach ($socialLinks as $SK => $SL) : ?>
-                                    <li>
-                                        <a href="<?= $SL ?>" target="_blank">
-                                            <i class="fa fa-<?= $SK ?>"></i>
-                                        </a>
-                                    </li>
-                                <?php endforeach; ?>
-
-                            </ul>
-                        </div>
                         <div class="nuws">
                             <h2>Newsletter Singup</h2>
-                            <p>Singup to recieve latest promotions</p>
+                            <p>Enter your email below and get informed of our offers, campaigns, new products alers!</p>
                             <form action="" method="POST" id="newslettersubmit" class="searchform _ilmForm">
                                 <input type="hidden" name="newsletter_add" value="1" />
                                 <div class="flex">
@@ -135,7 +142,7 @@ $shippingMethods = $this->shippingMethods();
 
 <script src="<?= Models::asset("assets/_ilm_own/js/__ilm_page_func.js") ?>"></script>
 <script type="text/javascript">
-    lazyLoadInstance.update()
+lazyLoadInstance.update()
 </script>
 
 <?php
@@ -144,7 +151,7 @@ if ($notification) :
     Session::Destroy("msg");
 ?>
     <script type="text/javascript">
-        showPageAlert("Alert !", "<?= htmlspecialchars($notification) ?>");
+    showPageAlert("Alert !", "<?= htmlspecialchars($notification) ?>");
     </script>
 <?php endif; ?>
 
