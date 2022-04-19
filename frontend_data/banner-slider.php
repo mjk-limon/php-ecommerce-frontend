028ec53a9d2aee9117a6b47098d3078bc2c0db31
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-04-19 15:14:59 +0600

    Update banner-slider.php

--- frontend_data/banner-slider.php
+++ frontend_data/banner-slider.php
@@ -1,6 +1,8 @@
 <?php
 $categorySample = array(
-    "Category background" => array("total" => 1, "type" => "png", "width" => "90", "height" => "25"),
+    "Category icon"    => array("total" => 1, "type" => "png", "width" => "19", "height" => "19"),
+    "Top ranking category icon"    => array("total" => 1, "type" => "png", "width" => "300", "height" => "300"),
+    "Homepage category sample" => array("total" => 1, "type" => "jpg", "width" => "600", "height" => "430"),
     "Product page banner" => array("total" => 3, "type" => "jpg", "width" => "900", "height" => "300")
 );
 
@@ -10,17 +12,17 @@ $innerPages = array(
 );
 
 $bannerToEdit = array(
-    "home page banners" => array("page" => 'index', "position" => 1, "fields" => "2"),
-    "instagram feeds" => array("page" => 'index', "position" => 7, "fields" => "3", "scale" => "600,600"),
-    "our stores" => array("page" => 'index', "position" => 8, "fields" => "3_-_Store title,5_-_Store location", "scale" => "600,600")
+    "home page banners" => array("page" => "index", "position" => 1, "fields" => "2")
 );
 
-$stickersToEdit = array(
-    "home page stickers top 1" => array("page" => 'index', "position" => 2, "fields" => "2,3", "scale" => "980,560"),
-    "home page stickers top 2" => array("page" => 'index', "position" => 3, "fields" => "2,3", "scale" => "980,560"),
-    "home page stickers 3" => array("page" => 'index', "position" => 4, "fields" => "2,3", "scale" => "1170,400")
+$stickersToEdit    = array(
+    "home page stickers top 1" => array("page" => "index", "position" => 2, "fields" => "2", "scale" => "450,215"),
+    "home page stickers new arrival" => array("page" => "index", "position" => 3, "fields" => "2", "scale" => "460,700"),
+    "offer text" => array("page" => "index", "position" => 4, "fields" => "5_-_Link Title", "omit_img" => true)
 );
 
 $stickersToEdit_NoDb = array();
 
-$addPrOthers = array();
+$addPrOthers = array(
+    "SKU" => ["type" => "text", "name" => "sku"]
+);
