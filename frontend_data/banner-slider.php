a942a8878a45aefa1006a8b333f2c022455064de
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-04-19 16:11:27 +0600

    Update banner-slider.php

--- frontend_data/banner-slider.php
+++ frontend_data/banner-slider.php
@@ -1,8 +1,7 @@
 <?php
 $categorySample = array(
-    "Category icon"    => array("total" => 1, "type" => "png", "width" => "19", "height" => "19"),
-    "Top ranking category icon"    => array("total" => 1, "type" => "png", "width" => "300", "height" => "300"),
-    "Homepage category sample" => array("total" => 1, "type" => "jpg", "width" => "600", "height" => "430"),
+    "Category icon" => array("total" => 1, "type" => "png", "width" => "19", "height" => "19"),
+    "Category icon 2" => array("total" => 1, "type" => "png", "width" => "300", "height" => "300"),
     "Product page banner" => array("total" => 3, "type" => "jpg", "width" => "900", "height" => "300")
 );
 
@@ -15,14 +14,11 @@ $bannerToEdit = array(
     "home page banners" => array("page" => "index", "position" => 1, "fields" => "2")
 );
 
-$stickersToEdit    = array(
-    "home page stickers top 1" => array("page" => "index", "position" => 2, "fields" => "2", "scale" => "450,215"),
-    "home page stickers new arrival" => array("page" => "index", "position" => 3, "fields" => "2", "scale" => "460,700"),
-    "offer text" => array("page" => "index", "position" => 4, "fields" => "5_-_Link Title", "omit_img" => true)
+$stickersToEdit = array(
+    "home page stickers 1" => array("page" => "index", "position" => 2, "fields" => "2", "scale" => "960,300"),
+    "home page stickers 2" => array("page" => "index", "position" => 3, "fields" => "2", "scale" => "960,300")
 );
 
 $stickersToEdit_NoDb = array();
 
-$addPrOthers = array(
-    "SKU" => ["type" => "text", "name" => "sku"]
-);
+$addPrOthers = array();
