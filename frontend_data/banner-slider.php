3bf32a5f32da132d767ecf2860921ae97c2e15fd
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-04-19 15:28:26 +0600

    Update banner-slider.php

--- frontend_data/banner-slider.php
+++ frontend_data/banner-slider.php
@@ -1,7 +1,7 @@
 <?php
 $categorySample = array(
-    "Category background" => array("total" => 1, "type" => "png", "width" => "90", "height" => "25"),
-    "Product page banner" => array("total" => 3, "type" => "jpg", "width" => "900", "height" => "300")
+    "category background" => array("total" => 1, "type" => "png", "width" => "90", "height" => "25"),
+    "Product page banner" => array("total" => 1, "type" => "jpg", "width" => "1920", "height" => "130")
 );
 
 $innerPages = array(
@@ -11,14 +11,11 @@ $innerPages = array(
 
 $bannerToEdit = array(
     "home page banners" => array("page" => 'index', "position" => 1, "fields" => "2"),
-    "instagram feeds" => array("page" => 'index', "position" => 7, "fields" => "3", "scale" => "600,600"),
-    "our stores" => array("page" => 'index', "position" => 8, "fields" => "3_-_Store title,5_-_Store location", "scale" => "600,600")
+    "home page top slider" => array("page" => 'index', "position" => 3, "fields" => "2", "scale" => "600,600")
 );
 
 $stickersToEdit = array(
-    "home page stickers top 1" => array("page" => 'index', "position" => 2, "fields" => "2,3", "scale" => "980,560"),
-    "home page stickers top 2" => array("page" => 'index', "position" => 3, "fields" => "2,3", "scale" => "980,560"),
-    "home page stickers 3" => array("page" => 'index', "position" => 4, "fields" => "2,3", "scale" => "1170,400")
+    "home page stickers top 1" => array("page" => 'index', "position" => 2, "fields" => "2", "scale" => "1170,250")
 );
 
 $stickersToEdit_NoDb = array();
