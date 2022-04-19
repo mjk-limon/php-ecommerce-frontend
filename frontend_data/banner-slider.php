03932a03ba7b73dcf51ef981249788cc59008ada
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-04-19 15:08:17 +0600

    frontend data, banner-slider added

---
+++ frontend_data/banner-slider.php
@@ -0,0 +1,26 @@
+<?php
+$categorySample = array(
+    "Category background" => array("total" => 1, "type" => "png", "width" => "90", "height" => "25"),
+    "Product page banner" => array("total" => 3, "type" => "jpg", "width" => "900", "height" => "300")
+);
+
+$innerPages = array(
+    "about-us", "store-location", "term-of-use", "privacy-policy", "testimonials", "blog",
+    "photo-confirmations", "payment-methods", "locations-we-ship-to", "shipping-returns"
+);
+
+$bannerToEdit = array(
+    "home page banners" => array("page" => 'index', "position" => 1, "fields" => "2"),
+    "instagram feeds" => array("page" => 'index', "position" => 7, "fields" => "3", "scale" => "600,600"),
+    "our stores" => array("page" => 'index', "position" => 8, "fields" => "3_-_Store title,5_-_Store location", "scale" => "600,600")
+);
+
+$stickersToEdit = array(
+    "home page stickers top 1" => array("page" => 'index', "position" => 2, "fields" => "2,3", "scale" => "980,560"),
+    "home page stickers top 2" => array("page" => 'index', "position" => 3, "fields" => "2,3", "scale" => "980,560"),
+    "home page stickers 3" => array("page" => 'index', "position" => 4, "fields" => "2,3", "scale" => "1170,400")
+);
+
+$stickersToEdit_NoDb = array();
+
+$addPrOthers = array();
