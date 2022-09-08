ec3d6168370b4a1a04b216bf3168dd3d30e3fbab
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-08 20:43:55 +0600

    Layout Modernizer: global\javascripts\details_core

---
+++ mainpanel/layouts/global/javascripts/detailspage_core.php
@@ -0,0 +1,18 @@
+<script defer src="<?php echo asset("assets/_ilm_own/js/detailsPage_scripts.js") ?>"></script>
+
+<script defer type="text/javascript">
+$(document).ready(function() {
+    <?php if ($this->checkJump('jumpToRvw')) : ?>
+        $('#DRRvwBtn').trigger("click");
+        _ilm.jumpToSection('#DRRvwBtn', function() {
+            $('#rv-main-area .media:first-child').addClass('animated flash');
+        });
+    <?php endif; ?>
+
+    <?php if ($this->checkJump('jumpToQstn')) : ?>
+        _ilm.jumpToSection('#Rating', function() {
+            $('#rv-qus-area .media:first-child').addClass('animated flash');
+        });
+    <?php endif; ?>
+});
+</script>
