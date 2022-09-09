bbabb6875aca17c0c418f9f149e02073f6fdc1af
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-09 13:03:50 +0600

    Layout Modernizer: global\javascripts\mainpanel_foot_init

---
+++ mainpanel/layouts/global/javascripts/mainpanel_foot_init.php
@@ -0,0 +1,18 @@
+<?php
+
+use _ilmComm\Core\LocalStorage\Session;
+
+?>
+
+<script src="<?php echo asset("assets/_ilm_own/js/__ilm_page_func.js") ?>"></script>
+<script type="text/javascript">
+lazyLoadInstance.update();
+
+<?php
+if ($notification = Session::get("msg")) :
+    // Destroy session notification
+    Session::destroy("msg");
+?>
+    showPageAlert("Alert !", "<?php echo htmlspecialchars($notification) ?>");
+<?php endif; ?>
+</script>
