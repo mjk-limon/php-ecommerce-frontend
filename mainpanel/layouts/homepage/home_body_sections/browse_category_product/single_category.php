7c5b0ac1dab24fdce817ee120b983eea6c216e32
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-10 17:54:34 +0600

    Layout Modernizer: homepage\home_body\browse_category

---
+++ mainpanel/layouts/homepage/home_body_sections/browse_category_product/single_category.php
@@ -0,0 +1,23 @@
+<div class="bc-single">
+    <div class="bc-cat-name"><?php echo htmlspecialchars($Cat->Mainc) ?></div>
+    <div class="grid-row">
+        <div class="grids large-grid onlycolspan">
+            <div class="prgrid-ads-section">
+                <a href="<?php echo $Cat->getHref() ?>" class="prgrid-ad-img"
+                   style="background-image:url('<?php echo asset('images/category-slides/' . restyle_url($Cat->Mainc) . '-3.jpg') ?>')"></a>
+            </div>
+        </div>
+
+        <?php while ($TrPr = $BrowseCatProducts->fetch_assoc()) : ?>
+            <div class="grids">
+                <div class="bc-products">
+                    <?php $this->layout('global.single_product', ['sp' => $this->SingleProduct, 'pr_info' => $TrPr]); ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+
+    </div>
+    <div class="bc-cat-all">
+        <div>&nbsp;</div><a href="<?php echo $Cat->getHref() ?>">View All</a>
+    </div>
+</div>
