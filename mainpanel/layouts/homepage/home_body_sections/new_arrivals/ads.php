3801f3e8c0c5d4ad5e7ec112331cc9529298e599
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-10 17:54:44 +0600

    Layout Modernizer: homepage\home_body\new_arrival

---
+++ mainpanel/layouts/homepage/home_body_sections/new_arrivals/ads.php
@@ -0,0 +1,8 @@
+<?php if ($i == 2) : ?>
+    <div class="grids large-grid">
+        <div class="prgrid-ads-section">
+            <a href="<?php echo $this->TopSticker2['image_link'] ?>" class="prgrid-ad-img"
+               style="background-image:url('<?php echo $this->TopSticker2['image'] ?>')"></a>
+        </div>
+    </div>
+<?php endif; ?>
