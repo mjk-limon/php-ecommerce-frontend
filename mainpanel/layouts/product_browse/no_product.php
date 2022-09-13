7c07d2e8f10fd879c6b6b54f3dfe018a49a38894
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-13 11:56:28 +0600

    Layout Modernizer: product_browse

---
+++ mainpanel/layouts/product_browse/no_product.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="empty-product-page">
+            <img src="<?php echo asset("assets/images/pp-empty.png") ?>" alt="" />
+            <h4>WE CAN'T SEEM TO FIND THE PRODUCT YOU ARE LOOKING FOR</h4>
+            <h4>PLEASE HAVE A LOOK OUR OTHER CATEGORIES</h4>
+        </div>
+    </div>
+</div>
