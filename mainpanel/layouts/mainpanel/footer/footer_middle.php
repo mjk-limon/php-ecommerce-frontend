c6721b222593c9fe5571f8686f3ce9f578baf2e9
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-13 11:54:27 +0600

    Layout Modernizer: mainpanel\footer\footer_*

---
+++ mainpanel/layouts/mainpanel/footer/footer_middle.php
@@ -0,0 +1,11 @@
+<div class="footer-top">
+    <div class="container">
+        <div class="row">
+
+            <?php $this->layout('mainpanel.footer.footer_middle.footer_middle_left'); ?>
+            <?php $this->layout('mainpanel.footer.footer_middle.footer_middle_middle'); ?>
+            <?php $this->layout('mainpanel.footer.footer_middle.footer_middle_right'); ?>
+
+        </div>
+    </div>
+</div>
