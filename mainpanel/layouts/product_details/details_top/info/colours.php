f63b07ae488bef1a796a28d44dc0c254c64f0794
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-14 12:26:49 +0600

    recover deleted files 2

---
+++ mainpanel/layouts/product_details/details_top/info/colours.php
@@ -0,0 +1,25 @@
+<?php if ($Colors = array_filter($this->ProductDetails->getColors())) : ?>
+    <div class="pr-sc-area">
+        <div>Select Color:</div>
+        <ul class="color-selection">
+
+            <?php
+            foreach ($Colors as $Color) :
+                $resColor = restyle_url($Color, true);
+                $background = str_replace(" ", ", ", $Color, $count);
+                if ($count) {
+                    $background = 'linear-gradient(45deg, ' . $background . ')';
+                }
+                $colrPrev = (file_exists(doc_root("proimg/{$this->Prid}/{$resColor}-texture.png")) ?
+                    "url('" . asset("proimg/{$this->Prid}/{$resColor}-texture.png") . "') no-repeat center / 100% 100%" :
+                    $background
+                );
+            ?>
+                <li class="cs-btn">
+                    <i style="background:<?php echo $colrPrev ?>"></i> <?php echo $Color ?>
+                </li>
+            <?php endforeach; ?>
+
+        </ul>
+    </div>
+<?php endif; ?>
---
+++ mainpanel/layouts/product_details/details_top/info/title.php
@@ -0,0 +1,26 @@
+<div class="product-title">
+    <div class="pt-area">
+
+        <?php if (!$this->mobileView) : ?>
+            <h2 class="pr-name"><?php echo $this->ProductDetails->getName() ?></h2>
+        <?php endif; ?>
+
+        <small>
+            By, 
+            <a href="<?php echo '/search/?q=&a_s_t=brand&astval=' . urlencode($this->ProductDetails->getBrandName()) ?>">
+                <?php echo $this->ProductDetails->getBrandName() ?>
+            </a>
+        </small>
+
+        <p class="pr-price"
+           data-dis="<?php echo $this->ProductDetails->getDiscount() ?>"
+           data-prs="<?php echo round($this->ProductDetails->getPrice()) ?>">
+            <span><?php echo curr($this->ProductDetails->getPrice()) ?></span>
+
+            <?php if ($this->ProductDetails->getDiscount()) : ?>
+                <span class="pre-price"><?php echo curr($this->ProductDetails->getPrice(0)) ?></span>
+            <?php endif; ?>
+
+        </p>
+    </div>
+</div>
