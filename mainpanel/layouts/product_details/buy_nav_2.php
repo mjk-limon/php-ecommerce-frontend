b519c5edada05a6f21ca406f9935767b028b349f
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-14 12:26:56 +0600

    recover deleted files 3

---
+++ mainpanel/layouts/product_details/buy_nav_2.php
@@ -0,0 +1,7 @@
+<div class="m-flex">
+    <div class="qty-selection">
+        <?php $this->layout('product_details.buy_nav.quantity_selection') ?>
+    </div>
+    
+    <?php $this->layout('product_details.buy_nav.buttons') ?>
+</div>
