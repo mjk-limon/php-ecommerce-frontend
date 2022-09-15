be7d85e5186ddc040162e148219fd879f55743b6
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-15 19:10:14 +0600

    branch init

--- lib/branches/wellsellbd/mainpanel/layouts/product_details/buy_nav/total_amount.php
+++
@@ -1,7 +0,0 @@
-<p>
-    Product amount total:
-    <strong>
-        <?php echo curr() ?>
-        <span id="pramqty"><?php echo round($this->ProductDetails->getPrice()) ?></span>
-    </strong>
-</p>
