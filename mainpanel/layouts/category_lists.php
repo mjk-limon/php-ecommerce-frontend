7fbe89254d47d03ee917582c3a1beacf219961ac
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-09-25 11:33:04 +0600

    Layout Modernize: category_lists

--- mainpanel/layouts/category_lists.php
+++ mainpanel/layouts/category_lists.php
@@ -1,56 +1,53 @@
 <?php
 
 // Category 
-$ci = new _ilmComm\Category\CategoryInfo\CategoryInfo;
+$ci = new _ilmComm\Category\FetchCategories\AllCategories;
 
 $submenu_addClass = $this->mobileView ? 'animated slideDown' : null;
 $menu_clearfix = $this->mobileView ? 2 : 3;
 
-$MainCats = $ci->getFetchable()->fetchMain();
-while ($ArrMain = $MainCats->fetch_assoc()) {
-    $ci->setCatArr($ArrMain);
+$MainCats = $ci->fetch($ci::FETCH_MAIN);
+while ($ArrMain = $MainCats->fetch()) {
 ?>
     <li class="dropdown">
-        <a href="<?php echo $ci->getHref() ?>">
-            <span><img src="<?php echo current($ci->getCatImg("Category icon")) ?>"></span>
-            <?php echo htmlspecialchars($ci->getMain()) ?>
+        <a href="<?php echo $ArrMain->getHref() ?>">
+            <span><img src="<?php echo asset(current($ArrMain->getCatImg("Category icon"))) ?>"></span>
+            <?php echo htmlspecialchars($ArrMain->getMain()) ?>
             <i class="fa fa-angle-down"></i>
         </a>
         <ul role="menu" class="sub-menu <?php echo $submenu_addClass ?>">
             <div class="col-xs-12 sub-cols view-all hidden-md hidden-lg">
-                <a href="<?php echo $ci->getHref() ?>">
-                    View All <?php echo htmlspecialchars($ci->getMain()) ?>
+                <a href="<?php echo $ArrMain->getHref() ?>">
+                    View All <?php echo htmlspecialchars($ArrMain->getMain()) ?>
                     <i class="fa fa-long-arrow-right"></i>
                 </a>
             </div>
 
             <?php
             $col4i = 1;
-            $SubGroupCats = $ci->getFetchable()->fetchSubGroup();
+            $SubGroupCats = $ArrMain->getFetchable()->fetch($ci::FETCH_SUB);
             $TotalSubGroupCats = $SubGroupCats->num_rows;
 
-            while ($ArrSubGrp = $SubGroupCats->fetch_assoc()) {
-                $ci->setCatArr($ArrSubGrp);
+            while ($ArrSubGrp = $SubGroupCats->fetch()) {
             ?>
                 <div class="col-md-4 col-xs-6 sub-cols">
-                    <a href="<?php echo $ci->getHref() ?>">
-                        <h3><?php echo htmlspecialchars($ci->getSubGroup())  ?></h3>
+                    <a href="<?php echo $ArrSubGrp->getHref() ?>">
+                        <h3><?php echo htmlspecialchars($ArrSubGrp->getSubGroup())  ?></h3>
                     </a>
 
                     <?php
-                    $SubCats = $ci->getFetchable()->fetchSub();
+                    $SubCats = $ArrSubGrp->getFetchable()->fetch($ci::FETCH_SUB);
                     $TotalSubCats = $SubCats->num_rows;
 
-                    while ($ArrSub = $SubCats->fetch_array()) {
-                        $ci->setCatArr($ArrSub);
+                    while ($ArrSub = $SubCats->fetch()) {
                     ?>
                         <li>
-                            <a href="<?php echo $ci->getHref() ?>">
-                                <?php echo htmlspecialchars($ci->getSub()); ?>
+                            <a href="<?php echo $ArrSub->getHref() ?>">
+                                <?php echo htmlspecialchars($ArrSub->getSub()); ?>
                             </a>
                         </li>
                     <?php
-                        if ($TotalSubCats == 1  && empty($ci->getSub())) {
+                        if ($TotalSubCats == 1  && empty($ArrSub->getSub())) {
                             echo '<div class="no-catsub"></div>';
                         }
                     }
