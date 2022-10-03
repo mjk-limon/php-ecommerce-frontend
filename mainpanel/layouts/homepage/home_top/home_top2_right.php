290dd4f451be8d8b1670ea52882ad9711dd3f12b
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-03 13:38:06 +0600

    Layout Modernizr: from Category Modernizer

--- mainpanel/layouts/homepage/home_top/home_top2_right.php
+++ mainpanel/layouts/homepage/home_top/home_top2_right.php
@@ -6,21 +6,12 @@
                 <h4>Categories</h4>
             </div>
 
-            <?php
-            $TrCat = $this->TrendCategories;
-            $TcRes = $TrCat->fetchTrending();
-
-            while ($TC = $TcRes->fetch_array()) :
-                $TrCat->setCatId($TC['id']);
-                $TrCat->setMain($TC['main']);
-
-                $CatLink = $TrCat->getHref();
-                $CatImg = $TrCat->getCatImg('Top ranking category icon')[0];
-            ?>
+            <?php while ($tc = $this->TrendCategories->fetch()) : ?>
                 <div class="tc-single">
-                    <a href="<?php echo $CatLink ?>">
-                        <div class="tc-single-img" style="background-image:url('<?php echo $CatImg ?>')"></div>
-                        <div class="tc-single-title"><?php echo htmlspecialchars($TC['main']) ?></div>
+                    <a href="<?php echo $tc->getCategoryLink() ?>">
+                        <div class="tc-single-img"
+                             style="background-image:url('<?php echo current($tc->getCategoryImage('Top ranking category icon')) ?>')"></div>
+                        <div class="tc-single-title"><?php echo htmlspecialchars($tc->getMain()) ?></div>
                     </a>
                 </div>
             <?php endwhile; ?>
