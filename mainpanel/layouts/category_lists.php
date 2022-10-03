fee28b7cf5a1020eba299692e8ddbb6f67bd3d97
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-03 13:38:17 +0600

    Layout Modernizr: from Category Modernizer

--- mainpanel/layouts/category_lists.php
+++ mainpanel/layouts/category_lists.php
@@ -1,61 +1,62 @@
 <?php
 
-// Category 
-$ci = new _ilmComm\Category\FetchCategories\AllCategories;
+// Main categories
+$ci = $this->AllCategories;
+$MainCats = $ci->get($ci::FETCH_MAIN);
 
+// Menu classes and clearfixes
 $submenu_addClass = $this->mobileView ? 'animated slideDown' : null;
 $menu_clearfix = $this->mobileView ? 2 : 3;
 
-$MainCats = $ci->fetch($ci::FETCH_MAIN);
 while ($ArrMain = $MainCats->fetch()) {
+    // Sub group categories
+    $SubGroupCats = $ArrMain->getFetchable()->get($ci::FETCH_SUB);
+    $TotalSubGroupCats = $SubGroupCats->num_rows;
 ?>
     <li class="dropdown">
-        <a href="<?php echo $ArrMain->getHref() ?>">
-            <span><img src="<?php echo asset(current($ArrMain->getCatImg("Category icon"))) ?>"></span>
+        <a href="<?php echo $ArrMain->getCategoryLink() ?>">
+            <span><img src="<?php echo asset(current($ArrMain->getCategoryImage("Category icon"))) ?>"></span>
             <?php echo htmlspecialchars($ArrMain->getMain()) ?>
             <i class="fa fa-angle-down"></i>
         </a>
         <ul role="menu" class="sub-menu <?php echo $submenu_addClass ?>">
             <div class="col-xs-12 sub-cols view-all hidden-md hidden-lg">
-                <a href="<?php echo $ArrMain->getHref() ?>">
+                <a href="<?php echo $ArrMain->getCategoryLink() ?>">
                     View All <?php echo htmlspecialchars($ArrMain->getMain()) ?>
                     <i class="fa fa-long-arrow-right"></i>
                 </a>
             </div>
 
             <?php
+            // Init column break
             $col4i = 1;
-            $SubGroupCats = $ArrMain->getFetchable()->fetch($ci::FETCH_SUB);
-            $TotalSubGroupCats = $SubGroupCats->num_rows;
 
             while ($ArrSubGrp = $SubGroupCats->fetch()) {
+                // Sub categories
+                $SubCats = $ArrSubGrp->getFetchable()->get($ci::FETCH_SUB);
+                $TotalSubCats = $SubCats->num_rows;
             ?>
                 <div class="col-md-4 col-xs-6 sub-cols">
-                    <a href="<?php echo $ArrSubGrp->getHref() ?>">
+                    <a href="<?php echo $ArrSubGrp->getCategoryLink() ?>">
                         <h3><?php echo htmlspecialchars($ArrSubGrp->getSubGroup())  ?></h3>
                     </a>
 
-                    <?php
-                    $SubCats = $ArrSubGrp->getFetchable()->fetch($ci::FETCH_SUB);
-                    $TotalSubCats = $SubCats->num_rows;
-
-                    while ($ArrSub = $SubCats->fetch()) {
-                    ?>
+                    <?php while ($ArrSub = $SubCats->fetch()) { ?>
                         <li>
-                            <a href="<?php echo $ArrSub->getHref() ?>">
+                            <a href="<?php echo $ArrSub->getCategoryLink() ?>">
                                 <?php echo htmlspecialchars($ArrSub->getSub()); ?>
                             </a>
                         </li>
-                    <?php
-                        if ($TotalSubCats == 1  && empty($ArrSub->getSub())) {
-                            echo '<div class="no-catsub"></div>';
-                        }
-                    }
-                    $SubCats->free();
-                    ?>
+
+                        <?php if ($TotalSubCats == 1  && empty($ArrSub->getSub())) { ?>
+                            <div class="no-catsub"></div>
+                        <?php } ?>
+
+                    <?php } ?>
 
                 </div>
-            <?php
+
+                <?php
                 if ($col4i % $menu_clearfix == 0) {
                     echo '<div class="clearfix"></div>';
                 }
@@ -63,16 +64,14 @@ while ($ArrMain = $MainCats->fetch()) {
                 if (($TotalSubGroupCats == 1) && empty($Cat->SubGroup)) {
                     echo '<div class="no-cathead"></div>';
                 }
+                ?>
 
+            <?php
                 $col4i++;
             }
-            $SubGroupCats->free();
             ?>
 
             <div class="clearfix"></div>
         </ul>
     </li>
-<?php
-}
-$MainCats->free();
-?>
+<?php } ?>
