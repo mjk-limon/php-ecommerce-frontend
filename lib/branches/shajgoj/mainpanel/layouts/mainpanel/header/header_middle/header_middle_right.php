b042ffa5a2ac5799c3d7094dfc46c097f00e7715
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-12 12:01:24 +0600

    Branch Init: Layout (MainPanel\Header\Middle\Right)

---
+++ lib/branches/shajgoj/mainpanel/layouts/mainpanel/header/header_middle/header_middle_right.php
@@ -0,0 +1,32 @@
+<div class="col-md-4 ht-right deskv-hm-movable">
+    <div class="serachbox">
+        <form action="<?= PROJECT_FOLDER . 'search/' ?>" method="get">
+            <div class="searchfld deskv">
+                <input type="text" placeholder="Search for Proudcts, Brands..." name="q" autocomplete="off" class="input-text search-q" />
+                <button type="submit" class="subs"><i class="pe-7s-search subsi"></i></button>
+                <div id="search-suggestions" class="srch-datalist slimScroll"></div>
+            </div>
+        </form>
+    </div>
+    <div class="ht-right-icon icon-my-account">
+        <a class="cb chp db" href="/my-account/"><i class="pe-7s-user"></i></a>
+        <div class="header-floating-menu login-menu animated fadeInUp">
+            <ul class="nav">
+                <?php if (!$this->UserData) : ?>
+                    <li><a href="/login/">Login/Registration</a></li>
+                <?php else : ?>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">Order History</a></li>
+                    <li><a href="">Wishlists</a></li>
+                    <li><a href="">Logout</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+    <div class="ht-right-icon icon-shopping-bag">
+        <a class="cart-contents sc-btn" href="javascript:;" title="View your shopping cart">
+            <i class="pe-7s-shopbag"></i>
+            <span class="count">0</span>
+        </a>
+    </div>
+</div>
