c8ffb55b06801c7c5616eb196c333c5116c4abb4
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-12 12:01:06 +0600

    Branch Init: Layout (MainPanel\Header\Middle\Left)

---
+++ lib/branches/shajgoj/mainpanel/layouts/mainpanel/header/header_middle/header_middle_left.php
@@ -0,0 +1,7 @@
+<div class="col-md-2 col-xs-5 cols logo-cols">
+    <div class="logo">
+        <a href="/" id="home-btn">
+            <img src="<?php echo get_logo() ?>">
+        </a>
+    </div>
+</div>
