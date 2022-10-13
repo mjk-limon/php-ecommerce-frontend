c8bed5e15255ab8915d4bbab71ba3eece550c9ab
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-13 12:58:01 +0600

    Branch Init: Layout (MainPanel\Header\Bottom\Left)

---
+++ lib/branches/shajgoj/mainpanel/layouts/mainpanel/header/header_bottom/header_bottom_left.php
@@ -0,0 +1,9 @@
+<div class="col-md-12" id="all-dept-btn">
+    <div class="manue mainmenu cacaallpaje">
+        <ul class="nav navbar-nav">
+
+            <?php $this->layout('category_lists') ?>
+
+        </ul>
+    </div>
+</div>
