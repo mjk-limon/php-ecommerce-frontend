f2f6998cc4ac8336371aa6f88acbcbe35588a6bf
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-21 18:04:18 +0600

    branch init: home (body\ads2)

---
+++ lib/branches/style_echo/mainpanel/layouts/homepage/home_body_sections/ads2.php
@@ -0,0 +1,19 @@
+<div class="spd">
+    <div class="container">
+        <div class="section-mb">
+            <div class="prgrid-ads-section">
+                <div class="prgrid-ad-img">
+                    <a href="<?php echo $this->TopSticker3->getImageLink() ?>">
+                        <img src="<?php echo $this->TopSticker3->getImage() ?>" alt="Banner 1">
+                        <div class="img-overlay">
+                            <span><?php echo $this->TopSticker3->getImageHeading() ?></span>
+                            <span class="btn ilmbutton">
+                                <i class="pe-7s-play"></i> Watch Video
+                            </span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
