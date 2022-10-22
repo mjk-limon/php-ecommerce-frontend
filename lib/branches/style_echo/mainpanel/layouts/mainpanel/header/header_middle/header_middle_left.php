a9f4b2ab74f55268c8d3750efc8a59c73bb08242
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-22 15:44:24 +0600

    branch init: mainpanel (middle\left)

--- lib/branches/style_echo/mainpanel/layouts/mainpanel/header/header_middle/header_middle_left.php
+++ lib/branches/style_echo/mainpanel/layouts/mainpanel/header/header_middle/header_middle_left.php
@@ -1,7 +1,11 @@
-<div class="col-md-2 col-xs-5 cols logo-cols">
-    <div class="logo">
-        <a href="/" id="home-btn">
-            <img src="<?php echo get_logo() ?>">
-        </a>
-    </div>
+<div class="serachbox ht-right-item">
+    <form action="<?php echo PROJECT_FOLDER . 'search/' ?>" method="get">
+        <div class="searchfld deskv">
+            <input type="text" placeholder="Search for Proudcts, Brands..." name="q"
+                   class="input-text search-q"
+                   autocomplete="off" />
+            <button type="submit" class="subs"><i class="pe-7s-search subsi"></i></button>
+            <div id="search-suggestions" class="srch-datalist slimScroll"></div>
+        </div>
+    </form>
 </div>
