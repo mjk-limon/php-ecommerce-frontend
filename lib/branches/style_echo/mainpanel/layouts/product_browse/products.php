dc6e8e11191837184c201e1a447cf170967aa3f5
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-23 12:17:55 +0600

    branch init: product_browse (products)

--- lib/branches/style_echo/mainpanel/layouts/product_browse/products.php
+++ lib/branches/style_echo/mainpanel/layouts/product_browse/products.php
@@ -1,4 +1,4 @@
-<div class="col-md-8p5 col-md-9 product-main-panel">
+<div class="col-md-9 product-main-panel">
     <div class="section-mb features_items">
         <div id="pp-main-area" class="v3">
             <?php $this->layout('product_browse.products.top_title') ?>
