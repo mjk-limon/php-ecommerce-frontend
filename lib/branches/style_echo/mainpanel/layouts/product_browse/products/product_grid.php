79aaffb950d1555d005e36beccc16845002321f1
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-23 12:18:20 +0600

    branch init: product_browse (products\grid)

--- lib/branches/style_echo/mainpanel/layouts/product_browse/products/product_grid.php
+++ lib/branches/style_echo/mainpanel/layouts/product_browse/products/product_grid.php
@@ -1,5 +1,5 @@
 <div class="product-page-products">
-    <div class="grid-row grid3">
+    <div class="grid-row grid4">
         <?php foreach ($this->AllProducts as $Product) : ?>
             <div class="grids">
                 <?php $this->layout('global.single_product', ['sp' => $this->SingleProduct, 'pr_info' => $Product]); ?>
