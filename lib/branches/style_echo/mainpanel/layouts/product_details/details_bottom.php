eccfc0349f72af92f2f72b7869e6c2fac2788166
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-10-23 12:15:52 +0600

    branch init: product_details (bottom)

--- lib/branches/style_echo/mainpanel/layouts/product_details/details_bottom.php
+++ lib/branches/style_echo/mainpanel/layouts/product_details/details_bottom.php
@@ -3,9 +3,12 @@
         <div class="section-mb">
             <div class="product-page-products" style="padding-left: 0;padding-right: 0;">
                 <h4>CUSTOMER ALSO VIEWED</h4>
-                <div class="grid-row grid4">
+                <div class="grid-row grid5">
 
-                    <?php while ($Rpr = $this->ProductSuggestion->fetch_array()) : ?>
+                    <?php
+                    $Suggestions = $this->productSuggestion(true, 10);
+                    while ($Rpr = $Suggestions->fetch_array()) :
+                    ?>
                         <div class="grids">
                             <?php $this->layout('global.single_product', ['sp' => $this->SingleProduct, 'pr_info' => $Rpr]); ?>
                         </div>
@@ -22,10 +25,10 @@
         <div class="section-mb">
             <div class="product-page-products" style="padding-left: 0;padding-right: 0;">
                 <h4>YOU MAY ALSO LIKE</h4>
-                <div class="grid-row grid4">
+                <div class="grid-row grid5">
 
                     <?php
-                    $Suggestions = $this->productSuggestion(false);
+                    $Suggestions = $this->productSuggestion(false, 10);
                     while ($Rpr = $Suggestions->fetch_array()) :
                     ?>
                         <div class="grids">
