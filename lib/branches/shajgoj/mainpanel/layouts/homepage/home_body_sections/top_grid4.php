baf27fe8a591f093f662f194c6bea66e051201c7
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-11-20 18:47:22 +0600

    Modrn branch: home_body (top_grid4)

---
+++ lib/branches/shajgoj/mainpanel/layouts/homepage/home_body_sections/top_grid4.php
@@ -0,0 +1,27 @@
+<div class="spd">
+    <div class="container">
+        <div class="section-browse-cat">
+            <div class="row">
+                
+                <?php
+                $Slider = $this->getSliders(7);
+                while ($ArrSlider = $Slider->fetch_array()) :
+                ?>
+                    <div class="col-md-3">
+                        <div class="single-layout-grid">
+                            <div class="slg-image">
+                                <a href="<?php echo $ArrSlider['image_link'] ?>">
+                                    <img src="<?php echo asset($ArrSlider['image']) ?>" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                endwhile;
+                $Slider->free();
+                ?>
+
+            </div>
+        </div>
+    </div>
+</div>
