6bb2e86552a3876b4a85b2cf4a3ca87b16649734
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2023-01-28 19:08:33 +0600

    Branch modrn: Header bottom left (Responsive)

--- lib/branches/style_echo/mainpanel/layouts/mainpanel/header/header_bottom/header_bottom_left.php
+++ lib/branches/style_echo/mainpanel/layouts/mainpanel/header/header_bottom/header_bottom_left.php
@@ -1,12 +1,27 @@
-<div class="col-md-12" id="all-dept-btn">
-    <div class="manue mainmenu cacaallpaje">
-        <ul class="nav navbar-nav">
+<?php if (!$this->mobileView) : ?>
 
-            <?php $this->layout('category_lists') ?>
+    <div class="col-md-12" id="all-dept-btn">
+        <div class="manue mainmenu cacaallpaje">
+            <ul class="nav navbar-nav">
 
-            <li><a href="/page/about-us/">About Us</a></li>
-            <li><a href="/contact/">Support</a></li>
+                <?php $this->layout('category_lists') ?>
 
-        </ul>
+                <li><a href="/page/about-us/">About Us</a></li>
+                <li><a href="/contact/">Support</a></li>
+
+            </ul>
+        </div>
     </div>
-</div>
+<?php else : ?>
+    <div class="col-xs-12">
+        <div class="serachbox">
+            <form action="<?php echo PROJECT_FOLDER . 'search/' ?>" method="get">
+                <div class="searchfld deskv">
+                    <input type="text" placeholder="Search for Proudcts, Brands..." name="q" autocomplete="off" class="input-text search-q" />
+                    <button type="submit" class="subs"><i class="pe-7s-search subsi"></i></button>
+                    <div id="search-suggestions" class="srch-datalist slimScroll"></div>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php endif; ?>
