0d09c1d7cf0d3d3d29a66761746ab4653a308fcd
mjk-limon <jhmasterlimon11@gmail.com>
2023-02-03 23:23:57 +0600

    Product browse layout load problem fix 1

--- mainpanel/layouts/product_browse/products/pp_main.php
+++ mainpanel/layouts/product_browse/products/pp_main.php
@@ -1,2 +1,5 @@
-<?php $this->layout('product_browse.products.top_title'); ?>
+<?php if ($this->onlyLoad != 'ppp-ws') :  ?>
+    <?php $this->layout('product_browse.products.top_title'); ?>
+<?php endif; ?>
+
 <?php $this->layout('product_browse.products.product_grids'); ?>
